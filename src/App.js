9d7037357600ab66cd922f534396ad6fce93b207
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-05-29 17:42:40 +0100

    components props + style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-    console.log("Book to add:", title);
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+
+    setBooks(updatedBooks);
   };
 
-  const editBook = () => {};
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
 
-  const deleteBook = () => {};
+    setBooks(updatedBooks);
+  };
+
+  const createBook = (title) => {
+    const updatedBooks = [
+      ...books,
+      { id: Math.round(Math.random() * 9999), title },
+    ];
+    setBooks(updatedBooks);
+  };
 
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,5 +1,27 @@
-function BookEdit() {
-  return <div>BookEdit</div>;
+import { useState } from "react";
+
+function BookEdit({ book, onEdit }) {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onEdit(book.id, title);
+  };
+
+  return (
+    <div>
+      <form className="book-edit" onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input value={title} onChange={handleChange} className="input" />
+        <button className="button is-primary">Save</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookEdit;
