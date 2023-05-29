3e641d807b17e54d4fa384230589715d77d41026
Rodrigo de Lascio <rodrigodelascio@gmail.com>
2023-05-29 13:34:07 +0100

    bookcreate component and styles updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+
 function App() {
-  return <div> Books App</div>;
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log("Book to add:", title);
+  };
+
+  const editBook = () => {};
+
+  const deleteBook = () => {};
+
+  return (
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
